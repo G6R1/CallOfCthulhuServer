1083758d14dbc7678479b1a9d0a316660ebd375d
unknown <Gerian109@yandex.ru>
2023-03-01 17:27:06 +0300

    initial dev

---
+++ src/main/java/ru/grndev/cthulhuserver/CthulhuServerApplication.java
@@ -0,0 +1,13 @@
+package ru.grndev.cthulhuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CthulhuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CthulhuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/grndev/cthulhuserver/CthulhuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.grndev.cthulhuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CthulhuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
