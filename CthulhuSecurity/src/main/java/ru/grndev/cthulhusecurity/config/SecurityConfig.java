64e727afab6a39a7486ff0f0d6b5ac3169039980
unknown <Gerian109@yandex.ru>
2023-06-15 17:03:42 +0300

    save start sec conf

---
+++ CthulhuSecurity/src/main/java/ru/grndev/cthulhusecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package ru.grndev.cthulhusecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
